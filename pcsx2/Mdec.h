449e6d8e5d7952b3c5d0fdea2487ecd05907e088
￴ <>
2023-02-05 16:58:11 +0000

    ￴

--- pcsx2/Mdec.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern void mdecInit();
-extern void mdecWrite0(u32 data);
-extern void mdecWrite1(u32 data);
-extern u32  mdecRead0();
-extern u32  mdecRead1();
-extern void psxDma0(u32 madr, u32 bcr, u32 chcr);
-extern void psxDma1(u32 madr, u32 bcr, u32 chcr);
--- pcsx2/RDebug/deci2_drfp.cpp
+++
@@ -1,6 +0,0 @@
-#include "Common.h"
-#include "deci2.h"
-
-extern char d2_message[100];
-
-void D2_DCMP(char *inbuffer, char *outbuffer, char *message){}
